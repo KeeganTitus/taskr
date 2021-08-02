c28b877cd2da0ca7b3f1a1bdd9221edfe3824e2b
KeeganTitus <titusk92@gmail.com>
2021-08-02 18:31:35 +0200

    Added task table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_175854) do
+ActiveRecord::Schema.define(version: 2021_08_02_161104) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
